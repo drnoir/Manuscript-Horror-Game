1f9a21f1a24f2115ffed1139d88e8d49030cd77b
drnoir <chrispgodber@gmail.com>
2026-05-04 00:01:24 +0100

    - invalid path - copilot suggested

---
+++ fix-invalid-path.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+cd '/c/Users/crisp/Documents/halloween/Manuscript Horror Game'
+git filter-branch --force --index-filter 'git ls-files -s | perl -pe "s#Areas \(HTML\)/Infirmary /index\.html#Areas (HTML)/Infirmary/index.html#g" | GIT_INDEX_FILE=\"$GIT_INDEX_FILE.new\" git update-index --index-info && mv \"$GIT_INDEX_FILE.new\" \"$GIT_INDEX_FILE\"' -- --all
